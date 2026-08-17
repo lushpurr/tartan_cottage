2d45da27c4211819045e727d2739610402771256
Gavin Will <gavinjwill@gmail.com>
2026-08-17 20:50:45 +0100

    Adds contact form UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,29 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [FormsModule]
 })
 export class App {
   protected readonly title = signal('tartan-cottage');
+
+  contactData = signal({
+    name: 'Gavin Will',
+    email: 'email@email.com',
+    message: 'Hello, I would like to get in touch with you.'
+  });
+
+  showForm = signal(false);
+
+  submitForm(){
+    console.log('Form submitted:', this.contactData());
+  }
+
+  toggleForm() {
+    this.showForm.set(!this.showForm());
+  }
 }
