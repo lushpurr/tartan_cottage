013a402876384f592c3388df2de0b744ea7921f5
Gavin Will <gavinjwill@gmail.com>
2026-08-17 21:31:44 +0100

    Form working

--- src/app/app.ts
+++ src/app/app.ts
@@ -21,7 +21,48 @@ export class App {
 
   submitForm(){
     console.log('Form submitted:', this.contactData());
+    if(this.contactData().name && this.contactData().email && this.contactData().message) {
+      // Here you can add your form submission logic, e.g., sending the data to a server
+      console.log('Form data is valid. Proceeding with submission...');
+    } else {
+      console.log('Form data is invalid. Please fill in all fields.');
+    }
+
+    const formData = new FormData();
+    formData.append('access_key', 'cad4e4cc-4129-47dc-a1c6-ceef769541db');
+    formData.append('name', this.contactData().name);
+    formData.append('email', this.contactData().email);
+    formData.append('message', this.contactData().message);
+
+    fetch('https://api.web3forms.com/submit', {
+      method: 'POST',
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
   }
+
+//   onSubmit() {
+//   if (this.form.invalid) return;
+
+//   const formData = new FormData();
+
+//   formData.append('access_key', 'YOUR_ACCESS_KEY');
+//   formData.append('name', this.form.value.name);
+//   formData.append('email', this.form.value.email);
+//   formData.append('message', this.form.value.message);
+
+//   fetch('https://api.web3forms.com/submit', {
+//     method: 'POST',
+//     body: formData
+//   })
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data);
+//   });
+// }
 
   toggleForm() {
     this.showForm.set(!this.showForm());
