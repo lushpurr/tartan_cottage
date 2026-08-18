39eafafbc2b14676f47ffd252f07504017f9f9c8
Gavin Will <gavinjwill@gmail.com>
2026-08-18 20:55:44 +0100

    Clear form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -20,14 +19,6 @@ export class App {
   showForm = signal(false);
 
   submitForm(){
-    console.log('Form submitted:', this.contactData());
-    if(this.contactData().name && this.contactData().email && this.contactData().message) {
-      // Here you can add your form submission logic, e.g., sending the data to a server
-      console.log('Form data is valid. Proceeding with submission...');
-    } else {
-      console.log('Form data is invalid. Please fill in all fields.');
-    }
-
     const formData = new FormData();
     formData.append('access_key', 'cad4e4cc-4129-47dc-a1c6-ceef769541db');
     formData.append('name', this.contactData().name);
@@ -41,30 +32,28 @@ export class App {
     .then(res => res.json())
     .then(data => {
       console.log(data);
+
+    })
+    .catch(err => {
+      console.error(err);
+    })
+    .finally(() => {
+      this.toggleForm();
+      this.clearForm();
     });
   }
-
-//   onSubmit() {
-//   if (this.form.invalid) return;
-
-//   const formData = new FormData();
-
-//   formData.append('access_key', 'YOUR_ACCESS_KEY');
-//   formData.append('name', this.form.value.name);
-//   formData.append('email', this.form.value.email);
-//   formData.append('message', this.form.value.message);
-
-//   fetch('https://api.web3forms.com/submit', {
-//     method: 'POST',
-//     body: formData
-//   })
-//   .then(res => res.json())
-//   .then(data => {
-//     console.log(data);
-//   });
-// }
-
   toggleForm() {
     this.showForm.set(!this.showForm());
+    if (!this.showForm()) {
+      this.clearForm();
+    }
+  }
+
+  clearForm(){
+    this.contactData.set({
+      name: '',
+      email: '',
+      message: ''
+    });
   }
 }
